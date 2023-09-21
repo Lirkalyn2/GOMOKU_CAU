5545f791de5f4ed2b444a098e2c00c0436c1c6a4
Adrien Moreau <adrien.moreau@epitech.eu>
2023-09-21 12:21:07 +0200

    [ADD] some error management

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ x = Board()
 
 while(InGame):
     x.display()
-    P1Piece = input("Where do you want to put your piece? ")
-    if (x.putPiece((int(P1Piece[1:]) - 1), (ord(P1Piece[0]) - 97), 1) is False): # needs try catch to avoid crash
-        print("Wrong move, P1Piece is not available please try again!")
+    P1Piece = input("Where do you want to put your piece? ").split()
+    if (len(P1Piece) != 2 # make sure that we have two coordinate.
+        or len(P1Piece[0]) != 1 # check that there is only one char where the letter should be for the column
+        or P1Piece[1].isdigit() is False # verify that we have a number for the row
+        or x.putPiece((int(P1Piece[1]) - 1), (ord(P1Piece[0]) - 97), 1) is False): # try to put the piece in place if it's possible
+        print("Wrong move, please try again!")
     print(x.winCheck(1, "1"))
     ## AI player
