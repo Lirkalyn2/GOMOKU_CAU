32c949438d642e57fe1b4e4c2a85bf6117e2522b
Adrien Moreau <adrien.moreau@epitech.eu>
2023-09-21 12:33:35 +0200

    [ADD] python can call libwinCheck.so

---
+++ Makefile
@@ -0,0 +1,24 @@
+##
+## EPITECH PROJECT, 2019
+## Makefile
+## File description:
+## Makefile
+##
+
+.PHONY	:	all $(NAME) clean fclean re
+
+NAME	=	ok
+
+all	:
+			make -C ./c
+
+$(NAME)	:
+			make -C ./c
+
+clean	:
+			make clean -C ./c
+
+fclean	:
+			make fclean -C ./c
+
+re		:	fclean $(NAME)
--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 
-from board import Board
+# from board import Board
 
-InGame = True
-x = Board()
+# InGame = True
+# x = Board()
 
-while(InGame):
-    x.display()
-    P1Piece = input("Where do you want to put your piece? ").split()
-    if (len(P1Piece) != 2 # make sure that we have two coordinate.
-        or len(P1Piece[0]) != 1 # check that there is only one char where the letter should be for the column
-        or P1Piece[1].isdigit() is False # verify that we have a number for the row
-        or x.putPiece((int(P1Piece[1]) - 1), (ord(P1Piece[0]) - 97), 1) is False): # try to put the piece in place if it's possible
-        print("Wrong move, please try again!")
-    print(x.winCheck(1, "1"))
-    ## AI player
+# while(InGame):
+#     x.display()
+#     P1Piece = input("Where do you want to put your piece? ").split()
+#     if (len(P1Piece) != 2 # make sure that we have two coordinate.
+#         or len(P1Piece[0]) != 1 # check that there is only one char where the letter should be for the column
+#         or P1Piece[1].isdigit() is False # verify that we have a number for the row
+#         or x.putPiece((int(P1Piece[1]) - 1), (ord(P1Piece[0]) - 97), 1) is False): # try to put the piece in place if it's possible
+#         print("Wrong move, please try again!")
+#     print(x.winCheck(1, "1"))
+#     ## AI player
+
+import ctypes
+import pathlib
+
+if __name__ == "__main__":
+    # Load the shared library into ctypes
+    libname = pathlib.Path().absolute() / "libwinCheck.so"
+    c_lib = ctypes.CDLL(libname)
+
+answer = c_lib.main()
+print(answer)
