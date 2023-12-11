0ce9979b544f04c7f92aa6c46c66fa0be7a97bf8
Adrien Moreau <adrien.moreau@epitech.eu>
2023-12-11 07:35:59 +0100

    [CHANGE] WinChecker to static class.

--- WinChecker.cpp
+++ WinChecker.cpp
@@ -1,25 +1,21 @@
 #include "WinChecker.hpp"
 
 
-WinChecker::WinChecker(std::vector<std::vector<char>> board, int pieceNumber)
-{
-    _board = board;
-    _pieceNumber = pieceNumber;
-}
+WinChecker::WinChecker() {}
 
 WinChecker::~WinChecker() {}
 
 
-bool WinChecker::rowCheck(std::vector<std::vector<char>> board)
+bool WinChecker::rowCheck(std::vector<std::vector<char>> board, int &pieceNumber)
 {
     int connection;
 
     for (std::size_t x = 0; x < board.size(); x++)
-        if (countOccurrences(board[x]))
+        if (countOccurrences(board[x], pieceNumber))
             for (std::size_t z = 0; z < (board.size() - 3); z++) {
                 connection = 0;
                 for (int c = 0; ((c < _goal) && ((z + c) < board[x].size())); c++) {
-                    if (board[x][(z + c)] == _pieceNumber)
+                    if (board[x][(z + c)] == pieceNumber)
                         connection++;
                     else
                         break;
@@ -30,235 +26,108 @@ bool WinChecker::rowCheck(std::vector<std::vector<char>> board)
     return false;
 }
 
-int WinChecker::countOccurrences(std::vector<char> arr)
+int WinChecker::countOccurrences(std::vector<char> &arr, int &pieceNumber)
 {
     int rsl = 0;
 
     for (std::size_t i = 0; i < arr.size(); i++)
-        if (arr[i] == _pieceNumber)
+        if (arr[i] == pieceNumber)
             rsl++;
     return rsl;
 }
 
-std::vector<std::vector<char>> WinChecker::transposeDiagonalDec()
+std::vector<std::vector<char>> WinChecker::transposeDiagonalDec(std::vector<std::vector<char>> &board)
 {
-    std::vector<std::vector<char>> lst(((_board.size() * 2) - 1));
+    std::vector<std::vector<char>> lst(((board.size() * 2) - 1));
 
-    for (std::size_t i = 0; i < ((_board.size() * 2) - 1); i++)
-        lst[i] = getDiagonalDec(i);
+    for (std::size_t i = 0; i < ((board.size() * 2) - 1); i++)
+        lst[i] = getDiagonalDec(board, i);
     return lst;
 }
 
-std::vector<char> WinChecker::getDiagonalDec(int digNum) {
+std::vector<char> WinChecker::getDiagonalDec(std::vector<std::vector<char>> &board, int digNum) {
     int index;
 
-    if (digNum <= (int)(_board.size() - 1)) {
-        index = (_board.size() - 1);
+    if (digNum <= (int)(board.size() - 1)) {
+        index = (board.size() - 1);
 
         std::vector<char> lst(digNum + 1);
 
         for (int i = digNum; i >= 0; i--) {
-            lst[i] = _board[i][index];
+            lst[i] = board[i][index];
             index--;
         }
         return lst;
     }
     else {
-        index = (((_board.size() * 2) - 2) - digNum);
+        index = (((board.size() * 2) - 2) - digNum);
 
-        std::vector<char> lst(_board.size());
+        std::vector<char> lst(board.size());
 
-        for (std::size_t i = (_board.size() - 1); i > (digNum - _board.size()); i--) {
-            lst[i] = _board[i][index];
+        for (std::size_t i = (board.size() - 1); i > (digNum - board.size()); i--) {
+            lst[i] = board[i][index];
             index--;
         }
         return lst;
     }
 }
 
-std::vector<std::vector<char>> WinChecker::transposeDiagonalInc()
+std::vector<std::vector<char>> WinChecker::transposeDiagonalInc(std::vector<std::vector<char>> &board)
 {
-    std::vector<std::vector<char>> lst((_board.size() * 2) - 1);
+    std::vector<std::vector<char>> lst((board.size() * 2) - 1);
 
-    for (std::size_t i = 0; i < ((_board.size() * 2) - 1); i++)
-        lst[i] = getDiagonalInc(i);
+    for (std::size_t i = 0; i < ((board.size() * 2) - 1); i++)
+        lst[i] = getDiagonalInc(board, i);
     return lst;
 }
 
-std::vector<char> WinChecker::getDiagonalInc(int digNum)
+std::vector<char> WinChecker::getDiagonalInc(std::vector<std::vector<char>> &board, int digNum)
 {
     int index;
 
-    if (digNum <= (int)(_board.size() - 1)) {
+    if (digNum <= (int)(board.size() - 1)) {
         index = 0;
 
         std::vector<char> lst(digNum + 1);
 
         for (int i = digNum; i >= 0; i--) {
-            lst[i] = _board[i][index];
+            lst[i] = board[i][index];
             index++;
         }
         return lst;
     }
     else {
-        index = (digNum - _board.size() + 1);
+        index = (digNum - board.size() + 1);
 
-        std::vector<char> lst(_board.size());
+        std::vector<char> lst(board.size());
 
-        for (std::size_t i = (_board.size() - 1); i > (digNum - _board.size()); i--) {
+        for (std::size_t i = (board.size() - 1); i > (digNum - board.size()); i--) {
             index++;
         }
         return lst;
     }
 }
 
-
-std::vector<std::vector<char>> WinChecker::transpose()
+std::vector<std::vector<char>> WinChecker::transpose(std::vector<std::vector<char>> &board)
 {
-    std::vector<std::vector<char>> lst(_board.size());
+    std::vector<std::vector<char>> lst(board.size());
 
-    for (std::size_t i = 0; i < _board.size(); i++)
-        lst[i] = getCol(i);
+    for (std::size_t i = 0; i < board.size(); i++)
+        lst[i] = getCol(board, i);
     return lst;
 }
 
-std::vector<char> WinChecker::getCol(int colNum)
+std::vector<char> WinChecker::getCol(std::vector<std::vector<char>> &board, int colNum)
 {
-    std::vector<char> lst(_board.size());
+    std::vector<char> lst(board.size());
 
-    for (std::size_t i = 0; i < _board.size(); i++) {
-        lst[i] = _board[i][colNum];
+    for (std::size_t i = 0; i < board.size(); i++) {
+        lst[i] = board[i][colNum];
     }
     return lst;
 }
 
-bool WinChecker::result()
+bool WinChecker::result(std::vector<std::vector<char>> &board, int pieceNumber)
 {
-    return (rowCheck(_board) || rowCheck(transpose()) || rowCheck(transposeDiagonalInc()) || rowCheck(transposeDiagonalDec()));
+    return (rowCheck(board, pieceNumber) || rowCheck(transpose(board), pieceNumber) || rowCheck(transposeDiagonalInc(board), pieceNumber) || rowCheck(transposeDiagonalDec(board), pieceNumber));
 }
-
-
-extern "C" {
-    bool winCheck(int pieceNumber, int **board) {
-        std::vector<std::vector<char>> boardPP(15);
-
-        for (int x = 0; x < 15; x++) {
-            std::vector<char> tmp(15);
-            for (int y = 0; y < 15; y++) {
-                tmp[y] = board[x][y];
-                // printf("%d", board[x][y]);
-            }
-            boardPP[x] = tmp;
-        }
-
-        return WinChecker(boardPP, pieceNumber).result();
-    }
-
-
-
-    // std::string ai(int **board) {
-    // void eeeeeee(int **board) {
-    //     std::vector<std::vector<char>> boardPP(15);
-
-    //     for (int x = 0; x < 15; x++) {
-    //         std::vector<char> tmp(15);
-    //         for (int y = 0; y < 15; y++) {
-    //             // tmp[y] = board[x][y];
-    //         //     // printf("%d", board[x][y]);
-    //         }
-    //         boardPP[x] = tmp;
-    //     }
-
-    //     // return ;
-
-    //     // uint256_t Player_1_bits = 0;
-    //     // uint256_t Player_2_bits = 0;
-
-    //     // for (int i = (int)(boardPP.size() - 1); i >= 0; i--) {
-    //     //     for(int j = (int)(boardPP[i].size() - 1); j >= 0; j--) {
-
-    //     //         if (boardPP[i][j] == 1)
-    //     //             Player_1_bits = (Player_1_bits << 1) | 1;
-    //     //         else
-    //     //             Player_1_bits = (Player_1_bits << 1);
-
-    //     //         if (boardPP[i][j] == 2)
-    //     //             Player_2_bits = (Player_2_bits << 1) | 1;
-    //     //         else
-    //     //             Player_2_bits = (Player_2_bits << 1);
-    //     //     }
-    //     // }
-
-    //     // AI test_player_2(boardPP, 2, 1);
-    //     // std::pair<int, int> rsl = test_player_2.bestMove(Player_1_bits, Player_2_bits);
-    //     // return std::to_string(rsl.first) + " " + std::to_string(rsl.second);
-
-    //     // return WinChecker(boardPP, pieceNumber).result();
-    // }
-}
-
-// int main(void)
-// {
-//     int **data;
-
-//     data = new int*[15];
-
-//     for (int x = 0; x < 15; x++)
-//         data[x] = new int[15];
-
-//     for (int x = 0; x < 15; x++)
-//         for (int y = 0; y < 15; y++)
-//             data[x][y] = 0;
-
-// /*
-//     data[0][0] = 1;
-//     data[0][1] = 1;
-//     data[0][2] = 1;
-//     data[0][3] = 1;
-//     data[0][4] = 1;
-// */
-
-// /*
-//     data[0][0] = 1;
-//     data[1][0] = 1;
-//     data[2][0] = 1;
-//     data[3][0] = 1;
-//     data[4][0] = 1;
-// */
-
-
-// /*
-//     data[0][4] = 1;
-//     data[1][3] = 1;
-//     data[2][2] = 1;
-//     data[3][1] = 1;
-//     data[4][0] = 1;
-// */
-
-
-// /*
-//     data[0][0] = 1;
-//     data[1][1] = 1;
-//     data[2][2] = 1;
-//     data[3][3] = 1;
-//     data[4][4] = 1;
-// */
-
-//     for (int x = 0; x < 15; x++) {
-//         for (int y = 0; y < 15; y++)
-//             std::cout << data[x][y] << " ";
-//         std::cout << std::endl;
-//     }
-
-//     std::cout << winCheck(1, data) << std::endl;
-//     std::cout << true << std::endl;
-
-
-//     for (int x = 0; x < 15; x++)
-//         delete[] data[x];
-
-//     delete[] data;
-
-
-//     return 0;
-// }
--- WinChecker.hpp
+++ WinChecker.hpp
@@ -6,27 +6,24 @@
 
 class WinChecker {
     public:
-        WinChecker(std::vector<std::vector<char>> board, int pieceNumber);
+        WinChecker();
         ~WinChecker();
-
-        bool rowCheck(std::vector<std::vector<char>> board);
-        int countOccurrences(std::vector<char> arr);
-
-        std::vector<std::vector<char>> transposeDiagonalDec();
-        std::vector<char> getDiagonalDec(int digNum);
-
-        std::vector<std::vector<char>> transposeDiagonalInc();
-        std::vector<char> getDiagonalInc(int digNum);
-
-        std::vector<std::vector<char>> transpose();
-        std::vector<char> getCol(int colNum);
-
-        bool result();
+        static bool result(std::vector<std::vector<char>> &board, int pieceNumber);
 
     protected:
     private:
-        std::vector<std::vector<char>> _board;
-        char _pieceNumber;
+        static bool rowCheck(std::vector<std::vector<char>> board, int &pieceNumber);
+        static int countOccurrences(std::vector<char> &arr, int &pieceNumber);
+
+        static std::vector<std::vector<char>> transposeDiagonalDec(std::vector<std::vector<char>> &board);
+        static std::vector<char> getDiagonalDec(std::vector<std::vector<char>> &board, int digNum);
+
+        static std::vector<std::vector<char>> transposeDiagonalInc(std::vector<std::vector<char>> &board);
+        static std::vector<char> getDiagonalInc(std::vector<std::vector<char>> &board, int digNum);
+
+        static std::vector<std::vector<char>> transpose(std::vector<std::vector<char>> &board);
+        static std::vector<char> getCol(std::vector<std::vector<char>> &board, int colNum);
+
         const static int _goal = 5;
 };
 
